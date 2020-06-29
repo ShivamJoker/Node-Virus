bc6da263f918f5855fffc30ffa88c94f1fe58a3b
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-06-29 21:29:11 +0530

    added robot js for mouse and keyboard control

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 const alert = require("alert");
 const open = require("open");
 const cpy = require("cpy");
+const brightness = require("brightness");
+const robot = require("robotjs");
 
 const run = async () => {
-  //   for (let i = 1; i < 11; i++) {
   // show user alert
-  alert(`Hello this is alert ${i}`);
+  // alert(`Hello this is alert ${i}`);
 
-  // Opens the URL in the default browser.
-  await open("http://example.com");
+  // // Opens the URL in the default browser.
+  // await open("http://example.com");
 
-  //open image
-  await open("wow-js.jpg");
+  // // open image
+  // await open("wow-js.jpg");
 
+  // // copying files
+  // await cpy(["*.jpg"], "../output/dist", { rename: `${i}.jpg`, parents: true });
+  // console.log("Files copied!");
 
-  //copying files
-  await cpy(["*.jpg"], "../output/dist", { rename: `${i}.jpg`, parents: true });
-  console.log("Files copied!");
-  //   }
+  // // change brightness to 0
+  // await brightness.set(0);
+
+  // moveMouse()
+
+  lockMac();
 };
 
-run();
+const moveMouse = () => {
+  // Speed up the mouse.
+  robot.setMouseDelay(2);
+
+  var twoPI = Math.PI * 2.0;
+  var screenSize = robot.getScreenSize();
+  var height = screenSize.height / 2 - 10;
+  var width = screenSize.width;
+
+  for (var x = 0; x < width; x++) {
+    y = height * Math.sin((twoPI * x) / width) + height;
+    robot.moveMouse(x, y);
+  }
+};
+
+setTimeout(() => {
+  robot.keyTap("q", ["command", "control"]);
+}, 2000);
+
+const lockMac = () => {
+  robot.keyTap("Q", ["command", "control"]);
+};
+
+// run();
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+  brightness.set(1).then(() => {
+//     console.log("Changed brightness to 80%");
+//   });
